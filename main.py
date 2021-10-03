625d35c911e85bb7faaac8a7af3992697ccd21f5
Senju <paulkpraveen@gmail.com>
2021-10-03 21:02:28 +0200

    calling functions to make the coffee machine work

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu=Menu()
+cash=MoneyMachine()
+coffee=CoffeeMaker()
+_ison=True
+while(_ison):
+    print("adad")
+    choice=input("What would you like? "+menu.get_items())
+    if(choice=="off"):
+        _ison=False
+    elif(choice=="report"):
+        coffee.report()
+    else:
+        drink=menu.find_drink(choice)
+        if(coffee.is_resource_sufficient(drink) and cash.make_payment(drink.cost)):
+            coffee.make_coffee(drink)
+
+
+
+
+
+
